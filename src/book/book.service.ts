54106eab6770b2119c5e173e8c33d131dd0f8208
modolee <modolee.si@gmail.com>
2024-01-02 14:36:46 +0900

    feat: 예매 시 예외사항 처리
    - 포인트 부족
    - 좌석 없음

--- src/book/book.service.ts
+++ src/book/book.service.ts
@@ -1,4 +1,8 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateBookDto } from './dtos/create-book.dto';
 import { Book } from './entities/book.entity';
 import { DataSource, Repository } from 'typeorm';
@@ -41,12 +45,21 @@ export class BookService {
       // 포인트 차감 -> show 가격 정보
       const price = schedule.show.price;
       const user = await queryRunner.manager.findOneBy(User, { id: userId });
-      user.points = user.points - price;
+
+      const afterPaidPoints = user.points - price;
+      if (afterPaidPoints < 0) {
+        throw new BadRequestException('포인트가 부족합니다.');
+      }
+      user.points = afterPaidPoints;
       await queryRunner.manager.save(User, user);
 
       // 좌석 개수 줄이기
       const seat = await queryRunner.manager.findOneBy(Seat, { scheduleId });
-      seat.availableSeats -= 1;
+      const afterBookedSeats = seat.availableSeats - 1;
+      if (afterBookedSeats < 0) {
+        throw new BadRequestException('예매 가능한 좌석이 없습니다.');
+      }
+      seat.availableSeats = afterBookedSeats;
       await queryRunner.manager.save(Seat, seat);
 
       await queryRunner.commitTransaction();
